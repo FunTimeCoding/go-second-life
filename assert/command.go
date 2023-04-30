7a857db8ebb56e66f12160389c6b3d56087ef979
Alexander Reitzel <funtimecoding@gmail.com>
2023-04-30 21:43:01 +0200

    link message and command asserts

---
+++ assert/command.go
@@ -0,0 +1,40 @@
+package assert
+
+import (
+	"github.com/funtimecoding/go-library/object_notation"
+	"github.com/funtimecoding/go-second-life/command"
+	"testing"
+)
+
+func Command(
+	t *testing.T,
+	expected *command.Command,
+	actual *command.Command,
+) {
+	t.Helper()
+	t.Logf("%s", object_notation.Format(actual))
+	expectedCount := len(expected.L)
+	actualCount := len(actual.L)
+
+	if actualCount != expectedCount {
+		t.Errorf(
+			"Message count"+
+				"\nExpected: %d"+
+				"\nActual:   %d",
+			expectedCount,
+			actualCount,
+		)
+	}
+
+	LinkMessages(t, expected.L, actual.L)
+
+	if actual.O != expected.O {
+		t.Errorf(
+			"Owner say"+
+				"\nExpected: %s"+
+				"\nActual:   %s",
+			expected.O,
+			actual.O,
+		)
+	}
+}
---
+++ assert/link_message.go
@@ -0,0 +1,50 @@
+package assert
+
+import (
+	"github.com/funtimecoding/go-second-life/link_message"
+	"testing"
+)
+
+func LinkMessage(
+	t *testing.T,
+	expected link_message.Message,
+	actual link_message.Message,
+) {
+	t.Helper()
+
+	if actual.N == expected.N {
+		t.Logf("PASS: Link event: %d", actual.N)
+	} else {
+		t.Errorf(
+			"FAIL: Number"+
+				"\nExpected: %d"+
+				"\nActual:   %d",
+			expected.N,
+			actual.N,
+		)
+	}
+
+	if actual.I == expected.I {
+		t.Logf("PASS: UUID: '%s'", actual.I)
+	} else {
+		t.Errorf(
+			"FAIL: Link identifier"+
+				"\nExpected: %s"+
+				"\nActual:   %s",
+			expected.I,
+			actual.I,
+		)
+	}
+
+	if actual.T == expected.T {
+		t.Logf("PASS: Text: '%s'", actual.T)
+	} else {
+		t.Errorf(
+			"FAIL: Link text"+
+				"\nExpected: %s"+
+				"\nActual:   %s",
+			expected.T,
+			actual.T,
+		)
+	}
+}
